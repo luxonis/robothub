5bbf56283c03970d8d544f282e7367369d9a78db
root <Daniil Pastukhov>
2022-12-04 21:22:10 +0100

    Update app.py

--- src/robothub_depthai/app.py
+++ src/robothub_depthai/app.py
@@ -6,3 +6,13 @@ class RobotHubApplication(robothub.RobotHubApplication):
     def __init__(self):
         super().__init__()
         self.camera_manager = HubCameraManager(self, robothub.DEVICES)
+
+    def start_execution(self):
+        self.camera_manager.start()
+
+    def stop(self):
+        self.camera_manager.stop()
+
+    @property
+    def hub_cameras(self):
+        return self.camera_manager.hub_cameras
