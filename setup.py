7fad880d08e63907d2322ad0911e06fa5be9f739
Daniil Pastukhov <plus79222222238@gmail.com>
2022-12-04 17:46:22 +0100

    Fix setup and package name

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import io
 from setuptools import setup
 
 setup(
-    name='robothub',
+    name='robothub_depthai',
     version='0.0.1',
     description='',
     long_description='',
@@ -12,9 +12,8 @@ setup(
     keywords='robothub robot hub connect agent depthai',
     author='Luxonis',
     author_email='support@luxonis.com',
-    packages=['robothub'],
+    packages=['robothub_depthai'],
     package_dir={"": "src"},  # https://stackoverflow.com/a/67238346/5494277
-    install_requires=required,
     include_package_data=True,
     project_urls={
         "Documentation": "https://docs.luxonis.com/",
