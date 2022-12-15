3cc211716a0a580d6ff635fcab18e8fef1e1831f
Daniil Pastukhov <pastudan@fit.cvut.cz>
2022-12-15 14:13:57 +0100

    Add multiple streams example

---
+++ examples/multiple_streams/app.py
@@ -0,0 +1,14 @@
+import robothub_depthai
+
+
+class ExampleApplication(robothub_depthai.RobotHubApplication):
+    def on_start(self):
+        for camera in self.hub_cameras:
+            color = camera.create_camera('color', resolution='1080p', fps=30)
+            nn = camera.create_nn('person-detection-retail-0013', input=color)
+            stereo = camera.create_stereo('800p', fps=30)
+
+            # It will automatically create a stream and assign matching callback based on Component type
+            camera.create_stream(component=color, unique_key=f'color_stream_{camera.id}', name='Color stream')
+            camera.create_stream(component=nn, unique_key=f'nn_stream_{camera.id}', name='Detections stream')
+            camera.create_stream(component=stereo, unique_key=f'depth_{camera.id}', name='Depth stream')
