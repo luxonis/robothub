cf3f4cd124f5b4cea11eff8d07648b44b86e757d
Daniil Pastukhov <pastudan@fit.cvut.cz>
2023-01-10 04:54:48 +0100

    Update default app

--- examples/default_app/app.py
+++ examples/default_app/app.py
@@ -10,9 +10,9 @@ class DefaultApplication(robothub_depthai.RobotHubApplication):
             if camera.has_color:
                 print(f'Initialized color stream with resolution: {color_resolution}')
                 color = camera.create_camera('color', resolution=color_resolution, fps=30)
-                camera.create_stream(component=color, unique_key=f'color_{camera.id}', name='Color stream')
+                camera.create_stream(component=color, unique_key=f'color_{camera.id}', name=f'Color stream {camera.id}')
 
             if camera.has_stereo:
                 print(f'Initialized depth stream with resolution: {mono_resolution}')
                 stereo = camera.create_stereo('color', resolution=mono_resolution, fps=30)
-                camera.create_stream(component=stereo, unique_key=f'depth_{camera.id}', name='Depth stream')
+                camera.create_stream(component=stereo, unique_key=f'depth_{camera.id}', name=f'Depth stream {camera.id}')
