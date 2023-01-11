61b10a6c58f4fcdccef330333ae8bd8dace88685
Daniil Pastukhov <pastudan@fit.cvut.cz>
2023-01-11 23:49:10 +0100

    Update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = io.open('README.md', encoding='utf-8').read()
 
 setup(
     name='robothub_depthai',
-    version='0.0.4',
+    version='0.0.5',
     description='RobotHub-DepthAI integration library',
     long_description=long_description,
     long_description_content_type='text/markdown',
