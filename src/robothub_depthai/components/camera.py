2d20d1cef9133bdadadfe5c1415a41db5eec2dea
Daniil Pastukhov <pastudan@fit.cvut.cz>
2023-03-03 02:00:54 +0100

    Move components to subpackage

---
+++ src/robothub_depthai/components/camera.py
@@ -0,0 +1,13 @@
+from robothub_depthai.components import Streamable
+
+__all__ = ['Camera']
+
+
+class Camera(Streamable):
+    def __init__(self, name: str, resolution: str, fps: int) -> None:
+        super().__init__()
+        self.name = name
+        self.resolution = resolution
+        self.fps = fps
+
+        self.camera_component = None  # type: depthai_sdk.components.CameraComponent
---
+++ src/robothub_depthai/components/stereo.py
@@ -0,0 +1,10 @@
+from robothub_depthai.components import Streamable
+
+
+class Stereo(Streamable):
+    def __init__(self, resolution: str, fps: int) -> None:
+        super().__init__()
+        self.resolution = resolution
+        self.fps = fps
+
+        self.stereo_component = None  # type: depthai_sdk.components.StereoComponent
