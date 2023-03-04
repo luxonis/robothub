fa9879f319fba6236cf3a12b53db9cc4ccd2e93e
Daniil Pastukhov <pastudan@fit.cvut.cz>
2023-03-04 17:38:24 +0100

    [WIP] Add stereo configuration

--- src/robothub_depthai/components/stereo.py
+++ src/robothub_depthai/components/stereo.py
@@ -1,4 +1,21 @@
+from enum import IntEnum
+from typing import Union
+
 from robothub_depthai.components.streamable import Streamable
+
+__all__ = ['Stereo']
+
+
+class DepthQuality(IntEnum):
+    FAST = 1  # nothing turned on
+    DEFAULT = 2  # lr check, median filter
+    QUALITY = 3  # lr check, subpixel
+
+
+class DepthRange(IntEnum):
+    SHORT = 1  # extended disparity
+    DEFAULT = 2  # no effect
+    LONG = 3  # subpixel
 
 
 class Stereo(Streamable):
@@ -7,4 +24,22 @@ class Stereo(Streamable):
         self.resolution = resolution
         self.fps = fps
 
+        self.quality = DepthQuality.DEFAULT
+        self.range = DepthRange.DEFAULT
+
         self.stereo_component = None  # type: depthai_sdk.components.StereoComponent
+
+    def configure(self, quality: Union[str, DepthQuality] = None, range: Union[str, DepthRange] = None) -> None:
+        if quality:
+            self.quality = self._set_enum_value(DepthQuality, quality)
+        if range:
+            self.range = self._set_enum_value(DepthRange, range)
+
+    @staticmethod
+    def _set_enum_value(enum, value):
+        if isinstance(value, str):
+            return enum[value.upper()]
+        elif isinstance(value, enum):
+            return value
+        else:
+            raise ValueError(f'Invalid value: {value}')
