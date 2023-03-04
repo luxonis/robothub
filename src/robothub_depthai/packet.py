da4e66118afc74359017dc2ed515514ff4f5a6fb
Daniil Pastukhov <pastudan@fit.cvut.cz>
2023-03-04 17:39:24 +0100

    [WIP] Add HubPacket wrapper for depthai-sdk packets

---
+++ src/robothub_depthai/packet.py
@@ -0,0 +1,17 @@
+import robothub
+
+from robothub_depthai import Device
+
+
+class HubPacket:
+    def __init__(self, device: Device, packet):
+        self.device = device
+        self.packet = packet
+
+    def upload_as_detection(self, title: str):
+        frame_bytes = self.packet.imgFrame.getData()
+        # TODO add metadata
+        robothub.DETECTIONS.send_frame_detection(imagedata=frame_bytes, title=title, camera_serial=self.device.mxid)
+
+    def upload_as_event(self, title):
+        raise NotImplementedError('Not implemented yet')
