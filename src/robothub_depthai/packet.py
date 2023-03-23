a7c8968737ac16c98922e2f0708bd913971caf40
Daniil Pastukhov <pastudan@fit.cvut.cz>
2023-03-23 20:08:19 +0100

    Update upload_as_detection method

--- src/robothub_depthai/packet.py
+++ src/robothub_depthai/packet.py
@@ -1,5 +1,10 @@
 import warnings
 
+try:
+    import cv2
+except ImportError:
+    cv2 = None
+
 import robothub
 
 
@@ -7,10 +12,13 @@ class HubPacket:
     def __init__(self, device: 'Device', packet):
         self.device = device
         self.packet = packet
+        self.__dict__.update(self.packet.__dict__)
 
     def upload_as_detection(self, title: str):
         try:
-            frame_bytes = self.packet.imgFrame.getData()
+            # convert numpy array to jpg
+            frame_bytes = cv2.imencode('.jpg', self.packet.frame)[1].tobytes()
+
             # TODO add metadata
             robothub.DETECTIONS.send_frame_detection(imagedata=frame_bytes, title=title, camera_serial=self.device.mxid)
         except Exception as e:
