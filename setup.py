78bb6791abaafb3b74014a3e30d72d1be1ded173
Daniil Pastukhov <pastudan@fit.cvut.cz>
2023-04-17 11:22:16 +0200

    Bump version to 1.0.4

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = io.open('README.md', encoding='utf-8').read()
 
 setup(
     name='robothub_oak',
-    version='1.0.3',
+    version='1.0.4',
     description='RobotHub-OAK integration library',
     long_description=long_description,
     long_description_content_type='text/markdown',
