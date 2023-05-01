d76b54a784d0c7ebeb403370661746b89b0b0fbf
Daniil Pastukhov <pastudan@fit.cvut.cz>
2023-05-01 23:19:14 +0200

    Rename upload_as_detection() to upload_as_event()

--- src/robothub_oak/packets.py
+++ src/robothub_oak/packets.py
@@ -21,16 +21,13 @@ class HubPacket:
 
         self.frame = self._packet.frame
 
-    def upload_as_detection(self, title: str):
+    def upload_as_event(self, title: str):
         try:
             # convert numpy array to jpg
             frame_bytes = cv2.imencode('.jpg', self._packet.frame)[1].tobytes()
             robothub.DETECTIONS.send_frame_detection(imagedata=frame_bytes, title=title, camera_serial=self.device.mxid)
         except Exception as e:
             warnings.warn(f'Could not upload detection with error: {e}')
-
-    def upload_as_event(self, title):
-        raise NotImplementedError('Not implemented yet')
 
 
 class DepthPacket(HubPacket):
