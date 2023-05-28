5aea562687ec436e20963c299b22908c30a4ca0b
Daniil Pastukhov <pastudan@fit.cvut.cz>
2023-05-28 17:01:29 +0200

    Add DetectionTrigger

--- src/robothub_oak/trigger_action/triggers.py
+++ src/robothub_oak/trigger_action/triggers.py
@@ -1,15 +1,32 @@
-from typing import Union, Callable
+from datetime import timedelta
+from typing import Union, Callable, Dict
 
 from robothub_oak.components import Stereo, NeuralNetwork, Camera
 
 __all__ = ['Trigger']
 
+from robothub_oak.packets import HubPacket
+
 
 class Trigger:
+    """
+    Generic trigger class. Can be used to create custom triggers via providing a condition function.
+    """
+
     def __init__(self,
                  component: Union[Camera, Stereo, NeuralNetwork],
-                 condition: Callable,
+                 condition: Callable[[HubPacket], bool],
                  cooldown: int = 30):
         self.component = component
         self.condition = condition
+        self.cooldown = cooldown
+
+
+class DetectionTrigger:
+    def __init__(self,
+                 input: NeuralNetwork,
+                 min_detections: Dict[str, int],
+                 cooldown: Union[timedelta, int]):
+        self.input = input
+        self.min_detections = min_detections
         self.cooldown = cooldown
