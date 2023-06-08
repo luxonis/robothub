b991be44650732ee92c689f73116cb5fb9863f1f
Daniil Pastukhov <pastudan@fit.cvut.cz>
2023-06-08 22:25:54 +0200

    Add type hints

--- src/robothub_oak/components/_component.py
+++ src/robothub_oak/components/_component.py
@@ -1,9 +1,9 @@
-from typing import Callable
+from typing import Callable, List
 
 
 class Component:
     def __init__(self):
-        self.callbacks = []
+        self.callbacks: List[Callable] = []
 
     def add_callback(self, callback: Callable) -> None:
         self.callbacks.append(callback)
