c37121e9608528c3ba541df3c620ed689df4117b
Daniil Pastukhov <pastudan@fit.cvut.cz>
2023-07-07 01:29:47 +0200

    Bump version to 1.4.1

--- src/robothub_oak/__init__.py
+++ src/robothub_oak/__init__.py
@@ -12,6 +12,6 @@ from .hub_camera import *
 from .manager import *
 from .utils import set_replay_path
 
-__version__ = '1.4.0'
+__version__ = '1.4.1'
 
 REPLAY_PATH = os.environ.get('RH_OAK_REPLAY_PATH', None)
