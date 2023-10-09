3d6b6253469584070567c705ae2a9e24ffd8c11a
Daniil Pastukhov <pastudan@fit.cvut.cz>
2023-10-09 13:24:34 +0200

    Add docstrings, add _catch_event_exception decorator

--- src/robothub/events.py
+++ src/robothub/events.py
@@ -1,16 +1,28 @@
 import logging
 import zipfile
+from datetime import datetime
 from io import BytesIO
-from pathlib import Path
 from typing import Union, Optional, List
 
 import cv2
 import numpy as np
 import robothub_core
 
-__all__ = ['send_image_event', 'send_frame_event_with_zipped_images']
+__all__ = ['send_image_event', 'send_frame_event_with_zipped_images', 'send_video_event']
 
 logger = logging.getLogger(__name__)
+
+
+def _catch_event_exception(func):
+    def wrapper(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except Exception as e:
+            logger.error(f'Failed to send event: {e}')
+            _log_event_status(False, "None")
+            return None
+
+    return wrapper
 
 
 def _log_event_status(result: bool, event_id):
@@ -20,6 +32,7 @@ def _log_event_status(result: bool, event_id):
         logger.info(f'Event {event_id}: failed to send.')
 
 
+@_catch_event_exception
 def send_image_event(image: Union[np.ndarray, bytes],
                      title: str,
                      device_id: str = None,
@@ -28,29 +41,36 @@ def send_image_event(image: Union[np.ndarray, bytes],
                      mjpeg_quality=98,
                      encode=False
                      ) -> Optional[str]:
-    """Send a single image frame event to RH."""
-    try:
-        if encode:
-            _, image = cv2.imencode(".jpg", image, [int(cv2.IMWRITE_JPEG_QUALITY), mjpeg_quality])
+    """
+    Send a single image frame event to RH.
 
-        event = robothub_core.EVENTS.prepare()
-        event.add_frame(bytes(image), device_id)
-        event.set_title(title)
-        if metadata:
-            event.set_metadata(metadata)
-        if tags:
-            event.add_tags(tags)
-        robothub_core.EVENTS.upload(event)
-        _log_event_status(True, event.id)
-        return event.id
-    except Exception as e:
-        logger.error(f'Failed to send event: {e}')
-        _log_event_status(False, "None")
-        return None
+    :param image: The image to send.
+    :param title: The title of the event.
+    :param device_id: The device ID to associate with the event.
+    :param metadata: A dictionary of metadata to associate with the event.
+    :param tags: A list of tags to associate with the event.
+    :param mjpeg_quality: The JPEG quality to use when encoding.
+    :param encode: Whether to encode the image as a JPEG before sending.
+    :return: The event ID. None if the event failed to send.
+    """
+    if encode:
+        _, image = cv2.imencode(".jpg", image, [int(cv2.IMWRITE_JPEG_QUALITY), mjpeg_quality])
+
+    event = robothub_core.EVENTS.prepare()
+    event.add_frame(bytes(image), device_id)
+    event.set_title(title)
+    if metadata:
+        event.set_metadata(metadata)
+    if tags:
+        event.add_tags(tags)
+    robothub_core.EVENTS.upload(event)
+    _log_event_status(True, event.id)
+    return event.id
 
 
-def send_frame_event_with_zipped_images(cv_frame,
-                                        files,
+@_catch_event_exception
+def send_frame_event_with_zipped_images(cv_frame: np.ndarray,
+                                        files: list,
                                         title: str,
                                         device_id: str,
                                         tags: List[str] = None,
@@ -58,48 +78,68 @@ def send_frame_event_with_zipped_images(cv_frame,
                                         encode: bool = False,
                                         mjpeg_quality=98
                                         ) -> Optional[str]:
-    """Send a collection of images as a single event to RH."""
-    try:
-        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), mjpeg_quality]
-        if encode:
-            _, cv_frame = cv2.imencode('.jpg', cv_frame, encode_param)
+    """
+    Send a collection of images as a single event to RH.
 
-        event = robothub_core.EVENTS.prepare()
-        event.add_frame(bytes(cv_frame), device_id)
-        event.set_title(title)
-        if tags:
-            event.set_tags(tags)
-        if metadata:
-            event.set_metadata(metadata)
-        logger.debug(f'Total files: {len(files)}')
-        with BytesIO() as zip_buffer:
-            with zipfile.ZipFile(zip_buffer, 'w') as zip_file:
-                for idx, file in enumerate(files):
-                    if encode:
-                        _, encoded = cv2.imencode('.jpg', file, encode_param)
-                    else:
-                        encoded = file
-                    # Convert the numpy array to bytes
-                    image_bytes = encoded.tobytes()
-                    # Add the bytes to the zip file with a unique filename
-                    filename = f'image_{idx}.jpeg'
-                    zip_file.writestr(filename, image_bytes)
-                    # event.add_file(bytes(encoded), name=f"image_{idx}.jpeg")
-            # Get the bytes of the zip file
-            zip_bytes = zip_buffer.getvalue()
-            event.add_file(zip_bytes, name=f'images.zip')
-            robothub_core.EVENTS.upload(event)
-            _log_event_status(True, event.id)
-            return event.id
-    except Exception as e:
-        logger.error(f'Failed to send event: {e}')
-        _log_event_status(False, 'None')
-        return None
+    :param cv_frame: The main image frame to send.
+    :param files: A list of images to zip and send.
+    :param title: The title of the event.
+    :param device_id: The device ID to associate with the event.
+    :param tags: A list of tags to associate with the event.
+    :param metadata: A dictionary of metadata to associate with the event.
+    :param encode: Whether to encode the images as JPEGs before sending.
+    :param mjpeg_quality: The JPEG quality to use when encoding.
+    :return: The event ID. None if the event failed to send.
+    """
+    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), mjpeg_quality]
+    if encode:
+        _, cv_frame = cv2.imencode('.jpg', cv_frame, encode_param)
+
+    event = robothub_core.EVENTS.prepare()
+    event.add_frame(bytes(cv_frame), device_id)
+    event.set_title(title)
+    if tags:
+        event.set_tags(tags)
+    if metadata:
+        event.set_metadata(metadata)
+    logger.debug(f'Total files: {len(files)}')
+    with BytesIO() as zip_buffer:
+        with zipfile.ZipFile(zip_buffer, 'w') as zip_file:
+            for idx, file in enumerate(files):
+                if encode:
+                    _, encoded = cv2.imencode('.jpg', file, encode_param)
+                else:
+                    encoded = file
+                # Convert the numpy array to bytes
+                image_bytes = encoded.tobytes()
+                # Add the bytes to the zip file with a unique filename
+                filename = f'image_{idx}.jpeg'
+                zip_file.writestr(filename, image_bytes)
+                # event.add_file(bytes(encoded), name=f"image_{idx}.jpeg")
+        # Get the bytes of the zip file
+        zip_bytes = zip_buffer.getvalue()
+        event.add_file(zip_bytes, name=f'images.zip')
+        robothub_core.EVENTS.upload(event)
+        _log_event_status(True, event.id)
+        return event.id
 
 
-def send_video_event(video, title: str):
-    if isinstance(video, str) or isinstance(video, Path):
-        with open(str(video), 'rb') as f:
-            robothub_core.DETECTIONS.send_video_event(video=f.read(), title=title)
-    else:
-        robothub_core.DETECTIONS.send_video_event(video=video, title=title)
+@_catch_event_exception
+def send_video_event(video: bytes | str,
+                     title: str,
+                     metadata: Optional[dict] = None
+                     ) -> Optional[str]:
+    """
+    Send a video event to RH. The video can be a path to a video file or a bytes object.
+
+    :param video: Path to a video file or a bytes object.
+    :param title: Title of the video event.
+    :param metadata: Overlay metadata to be displayed on the video.
+    :return: The event ID. None if the event failed to send.
+    """
+    event = robothub_core.EVENTS.prepare()
+    event.add_video(video, f'Video event {datetime.now().strftime("%Y-%m-%d %H:%M:%S")}', metadata)
+    event.set_title(title)
+    robothub_core.EVENTS.upload(event)
+    _log_event_status(True, event.id)
+    return event.id
