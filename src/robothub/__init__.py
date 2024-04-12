d91352a0f67c62396b29f83a23158a1ca98aa457
petrnovota <petr.novota@luxonis.com>
2024-04-13 00:53:49 +0200

    Release 2.5.7 - updated Replay - add     DeviceState = robothub_core.DeviceState     RobotHubDevice = robothub_core.RobotHubDevice     to robothub namespace

--- src/robothub/__init__.py
+++ src/robothub/__init__.py
@@ -19,7 +19,7 @@ try:
 except ImportError:
     import robothub.robothub_core_wrapper as robothub
 
-__version__ = "2.5.6"
+__version__ = "2.5.7"
 
 # Setup logging for the module
 # setup_logger(__name__)
